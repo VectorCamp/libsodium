4c1f748405f7be831530f899323caec399e4a02e
George Tsoulkanakis <george.tsoulkanakis@vectorcamp.gr>
2025-02-14 15:17:06 +0000

    WIP: removed register forcing (it now runs 482MB/s with inline assembly in load,g1,g2)

--- src/libsodium/crypto_generichash/blake2b/ref/blake2b-compress-neon.c
+++ src/libsodium/crypto_generichash/blake2b/ref/blake2b-compress-neon.c
@@ -31,38 +31,40 @@ static const uint64_t blake2b_IV[8] = {
     0xa54ff53a5f1d36f1ULL, 0x510e527fade682d1ULL, 0x9b05688c2b3e6c1fULL,
     0x1f83d9abfb41bd6bULL, 0x5be0cd19137e2179ULL
 };
-
-int
-blake2b_compress_neon(blake2b_state *S,
+            
+int blake2b_compress_neon(blake2b_state *S,
                       const uint8_t  block[BLAKE2B_BLOCKBYTES])
 {
-    register uint64x2_t row1l asm("v4");
-    register uint64x2_t row1h asm("v5");
-    register uint64x2_t row2l asm("v6");
-    register uint64x2_t row2h asm("v7");
+
+    uint64x2_t row1l;
+    uint64x2_t row1h;
+    uint64x2_t row2l;
+    uint64x2_t row2h;
     uint64x2_t row3l;
     uint64x2_t row3h;
-    register uint64x2_t row4l asm("v8");
-    register uint64x2_t row4h asm("v9");
-    register uint64x2_t b0 asm("v10");
-    register uint64x2_t b1 asm("v11");
-    register uint64x2_t t0 asm("v12");
-    register uint64x2_t t1 asm("v13");
-    register uint64x2_t t2 asm("v14");
-    register uint64x2_t t3 asm("v15");
-    register int8x16_t r16 asm("v24");
-    register int8x16_t r24 asm("v25");
+    uint64x2_t row4l;
+    uint64x2_t row4h;
+    uint64x2_t b0;
+    uint64x2_t b1;
+    uint64x2_t t0;
+    uint64x2_t t1;
+    uint64x2_t t2;
+    uint64x2_t t3;
+    int8x16_t r16;
+    int8x16_t r24;
 
     r16 = (int8x16_t){2, 3, 4, 5, 6, 7, 0, 1, 10, 11, 12, 13, 14, 15, 8, 9};
     r24 = (int8x16_t){3, 4, 5, 6, 7, 0, 1, 2, 11, 12, 13, 14, 15, 8, 9, 10};
-    register uint64x2_t m0 asm("v16");
-    register uint64x2_t m1 asm("v17");
-    register uint64x2_t m2 asm("v18");
-    register uint64x2_t m3 asm("v19");
-    register uint64x2_t m4 asm("v20");
-    register uint64x2_t m5 asm("v21");
-    register uint64x2_t m6 asm("v22");
-    register uint64x2_t m7 asm("v23");
+    
+
+    uint64x2_t m0;
+    uint64x2_t m1;
+    uint64x2_t m2;
+    uint64x2_t m3;
+    uint64x2_t m4;
+    uint64x2_t m5;
+    uint64x2_t m6;
+    uint64x2_t m7;
     m0 = vreinterpretq_u64_u8(vld1q_u8(block + 00));
     m1 = vreinterpretq_u64_u8(vld1q_u8(block + 16));
     m2 = vreinterpretq_u64_u8(vld1q_u8(block + 32));
@@ -71,7 +73,6 @@ blake2b_compress_neon(blake2b_state *S,
     m5 = vreinterpretq_u64_u8(vld1q_u8(block + 80));
     m6 = vreinterpretq_u64_u8(vld1q_u8(block + 96));
     m7 = vreinterpretq_u64_u8(vld1q_u8(block + 112));
-
     __asm__("label8_m:;"::);
     const uint64x2_t h0 = row1l = vld1q_u64(&S->h[0]);
     const uint64x2_t h1 = row1h = vld1q_u64(&S->h[2]);
